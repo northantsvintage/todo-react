ef896635897052551fa3f24c311e628959e406e1
Ida <northantsvintage@gmail.com>
2020-07-10 13:40:16 +0100

    rendering array of objects by turning each one into <Todo /> component

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App subject="Michael" />, document.getElementById('root'));
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Sleep", completed: false },
+    { id: "todo-2", name: "Repeat", completed: false }
+  ];
+
+// array available to the App component as props.tasks
+ReactDOM.render(<App tasks={DATA} />, document.getElementById('root'));
